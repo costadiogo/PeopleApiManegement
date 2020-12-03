a19eebd3c36bbb1a5f90c0604ea3d41ac643f1c5
PedroMateus26 <pedromateusneto@gmail.com>
2020-12-03 12:43:16 -0300

    Interface PersonMapper and listAll method added

--- src/main/java/com/restspring/pedromateus/controller/PersonController.java
+++ src/main/java/com/restspring/pedromateus/controller/PersonController.java
@@ -1,11 +1,13 @@
 package com.restspring.pedromateus.controller;
 
 import com.restspring.pedromateus.dto.MessageResponseDTO;
-import com.restspring.pedromateus.entity.Person;
+import com.restspring.pedromateus.dto.request.PersonDTO;
 import com.restspring.pedromateus.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -20,8 +22,13 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO getBook(@RequestBody Person person){
-        return personService.createPerson(person);
+    public MessageResponseDTO getBook(@RequestBody PersonDTO personDTO){
+        return personService.createPerson(personDTO);
 
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
     }
 }
--- src/main/java/com/restspring/pedromateus/service/PersonService.java
+++ src/main/java/com/restspring/pedromateus/service/PersonService.java
@@ -2,23 +2,35 @@ package com.restspring.pedromateus.service;
 
 
 import com.restspring.pedromateus.dto.MessageResponseDTO;
+import com.restspring.pedromateus.dto.request.PersonDTO;
 import com.restspring.pedromateus.entity.Person;
+import com.restspring.pedromateus.mapper.PersonMapper;
 import com.restspring.pedromateus.repository.PersonRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
     private PersonRespository personRespository;
+
+    private PersonMapper personMapper=PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRespository personRespository) {
         this.personRespository = personRespository;
     }
 
-    public MessageResponseDTO createPerson(Person person){
-        Person savedPerson=personRespository.save(person);
-        return MessageResponseDTO.builder().message("Created person with id: "+savedPerson.getId()).build();
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person personToSave=personMapper.toModel(personDTO);
+        Person savedPerson = personRespository.save(personToSave);
+        return MessageResponseDTO.builder().message("Created person with id: " + savedPerson.getId()).build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> list=personRespository.findAll();
+        return list.stream().map(personMapper::toDTO).collect(Collectors.toList());
     }
 }
